3f462e5cc7dcb5f92fca865dd72b639d0fcae20a
nagalakshmi.a <nagalakshmi.a@HYDPCM307695L.ad.infosys.com>
2024-01-01 13:47:51 +0530

    1st commit

---
+++ src/main/java/com/DbJpa/repository/PlayerRepository.java
@@ -0,0 +1,9 @@
+package com.DbJpa.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.DbJpa.entity.Player;
+
+public interface PlayerRepository extends CrudRepository<Player, Integer>{
+
+}
